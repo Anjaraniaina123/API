2e451deb4d5695fc84bb5bd328a7843a5b68d043
Anjaraniaina123 <hei.elysee@gmail.com>
2025-07-31 09:41:30 +0300

    création de POST/posts

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
+from typing import List
 from starlette.responses import Response, JSONResponse
 from starlette.responses import Response, JSONResponse
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class EventModel(BaseModel):
+    author : str
+    title : str
+    content : str
+    création_datetime : int
+
+
+
 
 # Q1 création de route GET/ping
 @app.get("/ping")
@@ -26,3 +37,20 @@ async def root(full_path: str):
         html_content = file.read()
     return Response(content=html_content, status_code = 404, media_type="text:html")
 
+
+# Q4 - création de POST/posts
+
+
+events_store: List[EventModel] = []
+
+
+def serialized_stored_events():
+    events_converted = []
+    for event in events_store:
+        events_converted.append(event.model_dump())
+    return events_converted
+
+@app.post("/posts")
+def new_events(event_payload: List[EventModel]):
+    events_store.extend(event_payload)
+    return {"posts": serialized_stored_events()}

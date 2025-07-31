bb61e64cdcd9f7b898118ded0ae14b9c1c185a52
Anjaraniaina123 <hei.elysee@gmail.com>
2025-07-31 08:59:40 +0300

    création de route get/ping, création de route get/home, création de configuration

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 from fastapi import FastAPI
+from starlette.responses import Response, JSONResponse
+from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
 
-
-@app.get("/")
+# Q1 création de route GET/ping
+@app.get("/ping")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "pong en text brute"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+# Q2- création de route GET/home
+@app.get("/home")
+async def say_welcome():
+    with open("welcome.html","r", encoding = "utf-8") as file: 
+        html_content = file.read()
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return Response(content=html_content, status_code = 200, media_type= "text/html")
+
+
+# Q3- configuration d'une application
+@app.get("/{full_path:path}")
+async def root(full_path: str):
+    with open("not_found.html", "r", encoding= "utf-8") as file:
+        html_content = file.read()
+    return Response(content=html_content, status_code = 404, media_type="text:html")
+
